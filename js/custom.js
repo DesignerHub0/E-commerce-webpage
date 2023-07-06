26672b1d9dbb93dcfca38c3680b7be956d9a2aa3
Ishan <ishan.majmudar50@gmail.com>
2023-07-06 22:53:58 +0530

    06072023 changes

--- js/custom.js
+++ js/custom.js
@@ -6,5 +6,18 @@ $(document).ready(function() {
         $("html, body").animate({ scrollTop: cls }, "slow");
 
     });
+    $('#product-slider').owlCarousel({
+        loop: true,
+        margin: 30,
+        dots: true,
+        nav: true,
+        items: 1,
+        responsive : {
+            // breakpoint from 768 up
+            768 : {
+                items: 1,
+            }
+        }
+    });
 
 });
